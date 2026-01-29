d325fefeda588f13682dc823ede9a149a387b2be
Antoni Jaros <antonijaros04@gmail.com>
2026-01-29 19:57:47 +0100

    init commit

---
+++ src/main/java/com/ajaros/discordaibot/DiscordAiBotApplication.java
@@ -0,0 +1,13 @@
+package com.ajaros.discordaibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordAiBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordAiBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ajaros/discordaibot/DiscordAiBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajaros.discordaibot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordAiBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
