7fd77216a171b0ef5057ad8d8b733826c7f4f694
Antoni Jaros <antonijaros04@gmail.com>
2026-01-29 19:59:35 +0100

    remove public modifier from main method

--- src/main/java/com/ajaros/discordaibot/DiscordAiBotApplication.java
+++ src/main/java/com/ajaros/discordaibot/DiscordAiBotApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DiscordAiBotApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(DiscordAiBotApplication.class, args);
     }
 
